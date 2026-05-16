1e58083627238888fa1c439a5bd45dfd980c96f1
Laura Sánchez <sanchezlaura.ing@gmail.com>
2026-05-17 01:54:52 +0200

    Add health check endpoint

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LauraSanchez.Portfolio.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult Get()
+        {
+            return Ok(new
+            {
+                status = "healthy",
+                timestamp = DateTime.UtcNow,
+                version = "1.0.0"
+            });
+        }
+    }
+}
